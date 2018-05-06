7bc6be854843e2a5c16fba70cfddc2803731696e
ksymmy <ksymmy@163.com>
2018-05-06 14:07:12 +0800

    修改代码

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("["+Thread.currentThread().getName()+"] "+"Hi World!");
+        System.out.println("["+Thread.currentThread().getName()+"]:"+"Hi World!");
     }
 }
