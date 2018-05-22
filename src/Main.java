2be731d44097ae78df5971d2627598622eca7c0e
ksymmy <ksymmy@163.com>
2018-05-22 20:22:38 +0800

    20.22

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("测试分支合并 feature-a");
         //git config --global push.default 'nothing'
         //提交feature-b
+        //提交feature-b2 时间20.22
     }
 }
