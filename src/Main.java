8b32096e682596561e5303c2168c65eff5d8481a
ksymmy <ksymmy@163.com>
2018-05-22 19:40:26 +0800

    feature-a

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("["+Thread.currentThread().getName()+"]:"+"Hi World!");
         System.out.println("测试分支合并");
-        System.out.println("测试分支合并2");
+        System.out.println("测试分支合并 feature-a");
     }
 }
