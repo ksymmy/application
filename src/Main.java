41670ea8fb129f5a784653d20bb732623549c8a7
ksymmy <ksymmy@163.com>
2018-05-22 16:08:49 +0800

    测试branch合并

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("["+Thread.currentThread().getName()+"]:"+"Hi World!");
+        System.out.println("测试分支合并");
     }
 }
